e77599af385f04be07ac5c521acfa379ab859183
Maibritt Rittermann Legart Jensen <maib1148@skivecollege.dk>
2021-02-05 14:40:01 +0100

    Added images

--- startup/routes.js
+++ startup/routes.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 module.exports = function (app) {
 
     app.use(cors());
-
+    app.use(express.static('public'));
     app.use(express.json());
     app.use('/api/customers', customers);
     app.use('/api/users', users);
