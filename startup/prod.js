8fb3e1acf636d03c6a45e5a2281bc5c6c0d9d7aa
Maibritt Rittermann <mri@skivecollege.dk>
2021-02-20 10:01:44 +0100

    Added new CORS settings

--- startup/prod.js
+++ startup/prod.js
@@ -6,26 +6,28 @@ module.exports = function (app) {
     app.use(helmet());
     app.use(compression());
 
-    app.use(function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        next();
-    });
+    // app.use(function(req, res, next) {
+    //     res.setHeader('Access-Control-Allow-Origin', '*');
+    //     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    //     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    //     res.setHeader('Access-Control-Allow-Credentials', true);
+    //     next();
+    // });
 
-    // const corsOpts = {
-    //     origin: '*',
+    const corsOpts = {
+        origin: '*',
       
-    //     methods: [
-    //       'GET',
-    //       'POST',
-    //     ],
+        methods: [
+          'GET',
+          'POST',
+          'PUT',
+          'DELETE'
+        ],
       
-    //     allowedHeaders: [
-    //       'Content-Type',
-    //     ],
-    // };
+        allowedHeaders: [
+          'Content-Type',
+        ],
+    };
       
-    // app.use(cors(corsOpts));
+    app.use(cors(corsOpts));
 }
